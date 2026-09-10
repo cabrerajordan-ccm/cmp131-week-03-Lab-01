1e205bc502a84a2c13ba62d97194ecf01900d3a2
cabrera.jordan <cabrerajordan-ccm cabrer.jordan@student.ccm.edu>
2026-09-10 11:16:37 -0400

    50% done with character assignment

--- character_profile.py
+++ character_profile.py
@@ -7,4 +7,6 @@ Name= "Spider-Man "
 Age=25
 print(Name,Age)
 occupation="Superhero"
-print("occupation")
+print(occupation)
+facts= "bitten by a radioactive spider, Has saved thousands of people, Has visited penty of universe" 
+print(facts)
