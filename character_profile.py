8a794debb673342c9a356555f44f8616706b8864
cabrera.jordan <cabrera.jordan@student.ccm.edu>
2026-09-17 23:34:01 -0400

    complete lab

--- character_profile.py
+++ character_profile.py
@@ -6,7 +6,7 @@
 Name= "Spider-Man "
 Age=25
 print(Name,Age)
-occupation="Superhero"
-print(occupation)
-facts= "bitten by a radioactive spider, Has saved thousands of people, Has visited penty of universe" 
-print(facts)
+occ="Superhero"
+print("occupation:",occ)
+fact= "bitten by a radioactive spider, Has saved thousands of people, Has visited penty of universe" 
+print("facts:",fact)
